5bef98a13a1d23ec008a103a8ae55950f6923635
Gooner11 <ngoctrung150695@gmail.com>
2017-06-30 11:32:33 +0700

    test fetch 2

--- app/src/main/java/giaodien/admin/new_btl_android_thue_truyen/MainActivity.java
+++ app/src/main/java/giaodien/admin/new_btl_android_thue_truyen/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity
     SQLiteManga db;
 
     // Master yep i am your master
-//    test fetch
+//    test fetch 2
     // Master
     @Override
     protected void onCreate(Bundle savedInstanceState) {
