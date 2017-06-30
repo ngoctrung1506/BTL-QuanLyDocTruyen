5d0a3a9ce64cdb5ebcda7d911117a9f0389cace8
Gooner11 <ngoctrung150695@gmail.com>
2017-06-30 11:21:33 +0700

    test conflict

--- app/src/main/java/giaodien/admin/new_btl_android_thue_truyen/MainActivity.java
+++ app/src/main/java/giaodien/admin/new_btl_android_thue_truyen/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity
     private GridLayoutManager lLayout;
     SQLiteManga db;
 
-    // Master
+    // Master yep i am your master
     // Master
     @Override
     protected void onCreate(Bundle savedInstanceState) {
